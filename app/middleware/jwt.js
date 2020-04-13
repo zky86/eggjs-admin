148ac92bae26ca15a74d2c0af85df693309a03d6
zky <272495222@qq.com>
2020-04-13 13:28:19 +0800

    添加路由token鉴权，Redis缓存

--- app/middleware/jwt.js
+++ app/middleware/jwt.js
@@ -8,21 +8,19 @@ module.exports = (options, app) => {
     if (authToken) {
       // authToken = authToken.substring(7);
       const res = verifyToken(authToken); // 解密获取的Token
-      console.log(res);
+      // console.log(res);
       if (res.userName) {
         // 如果需要限制单端登陆或者使用过程中废止某个token，或者更改token的权限。也就是说，一旦 JWT 签发了，在到期之前就会始终有效
         // 此处使用redis进行保存
-        console.log(res.userName);
         const redis_token = await ctx.app.redis.get(res.userName); // 获取保存的token
-        // console.log(redis_token);
         if (authToken === redis_token) {
           // ctx.locals.userName = res.userName;
           await next();
         } else {
-          ctx.body = { code: 480, message: '您的账号已在其他地方登录' };
+          ctx.body = { code: 482, message: '您的账号已在其他地方登录' };
         }
       } else {
-        ctx.body = { code: 481, message: '登录状态已过期' };
+        ctx.body = { code: 482, message: '登录状态已过期' };
       }
     } else {
       ctx.body = { code: 482, message: '请登陆后再进行操作' };
